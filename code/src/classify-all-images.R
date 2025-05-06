9cfcf89b81e76028ff2ce610c1f775bdf53c431a
Brian Yurk <yurk@hope.edu>
2025-05-06 14:39:08 -0400

    an old change

--- code/src/classify-all-images.R
+++ code/src/classify-all-images.R
@@ -1,7 +1,7 @@
 library(tictoc)
 source('code/src/spatial-utils.R')
 
-directories <- sprintf("raw_data/quadrats/quadrat%02d", seq(63, 83, 1)) 
+directories <- sprintf("raw_data/quadrats/quadrat%02d", seq(35, 62, 1)) 
 
 model_name <- 'xgb_model_final'
 model_path <- glue('outputs/{model_name}.rds')
